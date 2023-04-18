e2e2705e268b1728fb28bc8896e798e3bfbf686f
Himanshu Sharma <sirhimanshusharma003@gmail.com>
2023-04-19 00:53:00 +0530

    changes push

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ $(document).ready(function () {
       var scroll = $(window).scrollTop();
       if (scroll > 10) {
         console.log("Window Scrolled");
-        $(".navbar").css("background", "rgba(255, 255, 255)");
+        $(".navbar").css("background", "#f2f4f8");
+        // $(".navbar").css("background", "rgba(255, 255, 255)");
         // $("a.nav-link").css("color", "#fff");
     }else {
         console.log("Window Top")
-        $(".navbar").css("background", "rgba(255, 255, 255, 0.2)");
+        $(".navbar").css("background", "#f2f4f8");
+        // $(".navbar").css("background", "rgba(255, 255, 255, 0.2)");
         // $("a.nav-link").css("color", "#000");
       }
     })
@@ -20,4 +22,17 @@ $(document).ready(function () {
     $(".dynamicData").removeClass('basicSecondaryBtn');
     $("#"+btnId).addClass('basicSecondaryBtn');
   });
+
+
+  document.getElementsByClassName("dropdown-item")[0].addEventListener("mouseover", function(e) {
+    const parent = e.target.parentElement;
+    if (parent && parent.tagName === "LI" && parent.classList.contains("dropdown-item")) {
+        const parent = e.target.parentElement;
+        parent.children[1].classList.add("active");
+    }
+});
+    
+document.getElementsByClassName("dropdown")[0].addEventListener("mouseout", function(e) {
+    e.target.classList.remove("active");
+});
   
